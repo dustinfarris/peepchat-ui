d6fbe5eeac485ddf84c256c85d815b3fa1e2f8e5
Dustin Farris <dustin.farris@gmail.com>
2016-05-27 14:06:45 -0400

    part 5

--- app/routes/app/index.js
+++ app/routes/app/index.js
@@ -1,4 +1,47 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const { RSVP, Route } = Ember;
+
+export default Route.extend({
+
+  model() {
+    return RSVP.hash({
+      rooms: this.store.findAll('room'),
+      newRoom: { name: '', errors: [] }
+    });
+  },
+
+  actions: {
+    createRoom() {
+      // Get the "new room" model
+      let data = this.get('currentModel.newRoom');
+      // Create an ember-data record
+      let room = this.store.createRecord('room', { name: data.name });
+      // Clear any exisiting error messages
+      this.set('currentModel.newRoom.errors', []);
+
+      room.save().then(() => {
+        // Success!
+        this.get('flashMessages').success(`Created room: ${data.name}`);
+        this.set('currentModel.newRoom.name', '');  // Clear input
+      }).catch((response) => {
+        // Remove the ember data record from the store
+        this.store.unloadRecord(room);
+        // Pass any error messages along
+        this.set('currentModel.newRoom.errors', (response.errors || []).mapBy('detail'));
+        // Failure!
+        this.get('flashMessages').danger(`Problem creating room: ${data.name}`);
+      });
+    },
+
+    removeRoom(room) {
+      if (window.confirm('Are you sure?')) {
+        room.destroyRecord().then(() => {
+          this.get('flashMessages').success(`Deleted room: ${room.get('name')}`);
+        }).catch(() => {
+          this.get('flashMessages').danger(`Problem deleting room: ${room.get('name')}`);
+        });
+      }
+    }
+  }
 });
